0e45e32aa30f344cc01608603c987b7e803ac2df
hmasood21955 <hmasood878@gmail.com>
2023-10-19 22:10:00 +0500

    variable initionlization

--- dataPlan.c
+++ dataPlan.c
@@ -2,5 +2,12 @@
 #include<math.h>
 int main()
 {
+    float plan_gb = 30;
+    int current_day = 0;
+    float total_used_gb =0 ;
+    printf("enter the current day:");
+    scanf("%d",&current_day);
     
+    printf("enter the plan gb:");
+    scanf("%f",& total_used_gb);
 }
