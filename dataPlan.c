76edbad451b20338f6ac10f9ddb055b3e9117c41
hmasood21955 <hmasood878@gmail.com>
2023-10-19 22:14:28 +0500

    calculations

--- dataPlan.c
+++ dataPlan.c
@@ -10,4 +10,9 @@ int main()
     
     printf("enter the plan gb:");
     scanf("%f",& total_used_gb);
+    
+    int days_remaining = 30 - current_day + 1;
+    double avg_daily_usage = total_used_gb / current_day;
+    double avg_daily_remaining = (plan_gb - total_used_gb) / days_remaining;
+    
 }
